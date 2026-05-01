b8524eee6ace2e5aec508c1226fd64c9eb7c920a
eholmes83 <eholmes83@yahoo.com>
2026-05-01 15:18:12 -0400

    configuring and using redux for app state management for fetching products and error tracking

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,33 +1,23 @@
+import { useSelector, useDispatch } from "react-redux"
 import ProductCard from "./ProductCard"
 import { FaExclamationTriangle } from "react-icons/fa"
+import { useEffect } from "react"
+import { fetchProducts } from "../store/actions"
 
 const Products = () => {
-    const isLoading = false
-    const errorMessage = ""
 
-    const products = [{
-      productId: 652,
-      productName: "iPhone XS Max",
-      image: "https://placehold.co/600x400",
-      description: "Experience the latest in mobile technology with advanced cameras, powerful processing, and an all-day battery.",
-      quantity: 10,
-      price: 1450.0,
-      discount: 10.0,
-      specialPrice: 1305.0,
-    },
-    {
-      productId: 654,
-      productName: "MacBook Air M2s",
-      image: "https://placehold.co/600x400",
-      description: "Ultra-thin laptop with Apple's M2 chip, providing fast performance in a lightweight, portable design.",
-      quantity: 0,
-      price: 2550.0,
-      discount: 20.0,
-      specialPrice: 2040.0,
-    }]
+    const { isLoading, errorMessage } = useSelector(state => state.errors)
+    const { products } = useSelector(state => state.products)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchProducts())
+    },[dispatch])
+
+    console.log(products)
 
     return (
-        <div class='lg:px-14 sm:px-8 px-4 py-14 2xl:w-[90%] 2xl:mx-auto'>
+        <div className='lg:px-14 sm:px-8 px-4 py-14 2xl:w-[90%] 2xl:mx-auto'>
             { isLoading ? (
                 <p>Loading...</p>
             ) : errorMessage ? (
---
+++ src/store/reducers/ProductReducer.js
@@ -0,0 +1,25 @@
+const initialState = {
+    products: null,
+    categories: null,
+    pagination: {}
+}
+
+export const productReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'FETCH_PRODUCTS':
+            return {
+                ...state,
+                products: action.payload,
+                pagination: {
+                    ...state.pagination,
+                    pageNumber: action.pageNumber,
+                    pageSize: action.pageSize,
+                    totalPages: action.totalPages,
+                    totalElements: action.totalElements,
+                    lastPage: action.lastPage
+                }
+            }
+        default:
+            return state;
+    }
+}
